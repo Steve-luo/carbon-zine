bde8669db159916a260a6396a59c1ea32f92c671
Steve-luo <me@steve-luo.com>
2020-03-23 02:45:12 -0500

    added wait time

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function () {
-  document.getElementById("loader").style.opacity = 0
+  setTimeout(hideLoader(), 1000)
 })
+
+function hideLoader() {
+  document.getElementById("loader").style.opacity = 0
+}
